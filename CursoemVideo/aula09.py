455726ad5b378bdccfc317a9f87d753a802e831c
Patrick Ericksson <patrickericksson@gmail.com>
2023-02-05 19:37:49 -0300

    Exercises Update

---
+++ CursoemVideo/aula09.py
@@ -0,0 +1,13 @@
+frase = 'Curso em Vídeo Python'
+print(frase[3:13])
+print(frase[:13])
+print(frase[1:15])
+print(frase[1:15:3])
+print(frase.count('o'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(len(frase.strip()))
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print(frase.find('Vídeo'))
+print(frase.split())
--- PythonExercicios/ex019 copy.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-n1 = str(input('Primeiro Aluno: '))
-n2 = str(input('Segundo Aluno: '))
-n3 = str(input('Terceiro Aluno: '))
-n4 = str(input('Quarto Aluno: '))
-lista = [n1, n2, n3, n4]
-escolhido = choice(lista)
-print('O aluno sorteado foi {}'.format(escolhido))

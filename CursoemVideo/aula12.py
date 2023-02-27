fe6444430071c8e0aea7619e32dae1c95e064bb7
Patrick Ericksson <patrickericksson@gmail.com>
2023-02-26 22:25:23 -0300

    Exercises Update

---
+++ CursoemVideo/aula12.py
@@ -0,0 +1,10 @@
+nome = str(input('QUal é seu nome? '))
+if nome == 'Gustavo':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino')
+else:
+    print('Seu nome é bem normal')
+print('Tenhoa um bom dia, {}!'.format(nome))
---
+++ PythonExercicios/ex036.py
@@ -0,0 +1,1 @@
+print('Empréstimo')

d616d138fac34c101f8320744f85c32274a554dd
Patrick Ericksson <patrickericksson@gmail.com>
2023-03-01 21:35:46 -0300

    Exercises Update

--- PythonExercicios/ex048.py
+++ PythonExercicios/ex048.py
@@ -1,6 +1,8 @@
 s = 0
-for c in range(1,501):
-    if c % 2 != 0 and c % 3 ==0:
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
         print(c)
         s += c
-print('A soma dos números ímpares e múltiplos de 3 é {}'.format(s))
+        cont += 1
+print('A soma dos {} números ímpares e múltiplos de 3 é {}'.format(cont, s))
--- PythonExercicios/ex050.py
+++ PythonExercicios/ex050.py
@@ -1,6 +1,8 @@
 s = 0
+cont = 0
 for c in range(1, 7):
     n = int(input('Digite o {}º número: '.format(c)))
     if n % 2 == 0:
         s += n
-print('A soma dos números pares é {}'.format(s))
+        cont += 1
+print('A soma dos {} números pares é {}'.format(cont, s))

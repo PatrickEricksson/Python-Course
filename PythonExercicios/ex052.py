db0ea454eefffd329315af97658b47590c6ec33d
Patrick Ericksson <patrickericksson@gmail.com>
2023-03-03 10:22:41 -0400

    Exercises Update

--- PythonExercicios/ex052.py
+++ PythonExercicios/ex052.py
@@ -1,1 +1,16 @@
+tot = 0
 n = int(input('Digite um número inteiro: '))
+for c in range(1, n+1):
+    
+    if n % c == 0:
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{}'.format(c), end=' ')
+print('\n\033[mO número {} foi divisível {} vezes'.format(n, tot))
+if tot == 2:
+    print('E por isso ele é PRIMO')
+else:
+    print('E por isso ele NÃO É PRIMO')
+    

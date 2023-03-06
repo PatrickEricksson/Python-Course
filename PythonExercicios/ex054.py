c9ed06aac0c8c526d0938c36801210c689d0ecc7
Patrick Ericksson <patrickericksson@gmail.com>
2023-03-05 22:55:35 -0300

    Exercises Update

--- PythonExercicios/ex054.py
+++ PythonExercicios/ex054.py
@@ -1,12 +1,12 @@
 from datetime import date
-menor = 0
-maior = 0
+totmenor = 0
+totmaior = 0
 ano_atual = date.today().year
 for c in range(1, 8):
     ano_nasc = int(input('Digite o ano de nascimento da {}º pessoa: '.format(c)))
     if ano_atual - ano_nasc < 21:
-        menor = menor + 1
+        totmenor += 1
     else:
-        maior = maior + 1
-print('Menores de Idade: {}'.format(menor))
-print('Maiores de Idade: {}'.format(maior))
+        totmaior += 1
+print('Menores de Idade: {}'.format(totmenor))
+print('Maiores de Idade: {}'.format(totmaior))

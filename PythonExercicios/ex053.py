7c1ae31ea7feb66838c3444d437aafe144e3ad8d
Patrick Ericksson <patrickericksson@gmail.com>
2023-03-05 22:51:44 -0300

    Exercises Update

--- PythonExercicios/ex053.py
+++ PythonExercicios/ex053.py
@@ -1,1 +1,11 @@
-frase = str(input('Digite uma frase: '))
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+print('Você digitou a frase {}'.format(junto))
+inverso = ''
+for letra in range(len(junto)-1, -1, -1):
+    inverso += junto[letra]
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')

ed15afd84fbaf32ce4e1b053ffc9a772c956764b
Patrick Ericksson <patrickericksson@gmail.com>
2023-03-08 16:10:32 -0300

    Exercises Update

---
+++ PythonExercicios/ex065.py
@@ -0,0 +1,17 @@
+resp = 'S'
+media = soma = cont = 0
+while resp in 'Ss':
+    n = int(input('Digite um número: '))
+    soma += n
+    cont +=1
+    if cont == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / cont
+print('Você digitou {} números e a média foi {}'.format(cont, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))

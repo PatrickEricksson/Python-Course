0e63ec8eb0d8bba8b30f8896ae1b05e0ed3561d9
Patrick Ericksson <patrickericksson@gmail.com>
2023-04-11 18:02:56 -0300

    Exercises Update

--- PythonExercicios/ex072.py
+++ PythonExercicios/ex072.py
@@ -1,2 +1,11 @@
-print('CAIXA ELETRÔNICO')
-print('Novo Teste')
+cont = ('zero', 'um', 'dois', 'três', 'quatro',
+        'cinco', 'seis', 'sete', 'oito',
+        'nove', 'dez', 'onze', 'doze',
+        'treze', 'catorze', 'quinze', 'dezesseis',
+        'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente Novamente. ', end='')
+print(f'Você digitou o número {cont[num]}')

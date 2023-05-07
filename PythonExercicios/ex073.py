3cd32a601466d24c64a058a21d9117fad891cfab
Patrick Ericksson <patrickericksson@gmail.com>
2023-05-07 17:34:31 -0300

    Exercises Update

--- PythonExercicios/ex073.py
+++ PythonExercicios/ex073.py
@@ -1,3 +1,16 @@
-print('Teste de Exercício')
-from math import sqrt
-print(sqrt(81))
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense',
+         'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vitória', 'Curitiba', 'Avaí', 'Ponte Preta',
+         'Atlético-GO')
+print('-=' * 15)
+print(f'Lista de Times do Brasileirão: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são: {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos são: {times[-4:]}')
+print('-=' * 15)
+print(f'Times em Ordem Alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f'A Chapecoense está na {times.index("Chapecoense")+1}º posição')

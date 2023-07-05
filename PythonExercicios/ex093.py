1e82918722363868807edcc9b2519f21629c60f4
Patrick Ericksson <patrickericksson@gmail.com>
2023-07-05 16:45:08 -0300

    Exercises Update

--- PythonExercicios/ex093.py
+++ PythonExercicios/ex093.py
@@ -1,6 +1,18 @@
-dados = {}
-dados['nome'] = str(input('Nome do Jogador: '))
-dados['partidas'] = int(input(f'Quantas partidas {dados["nome"]} jogou? '))
-gols = ()
-for c in dados.items('partida'):
-    gols.append() = int(input(f'Quantos gols na partida {c}?'))
+jogador = {}
+partidas = []
+jogador['nome'] = str(input('Nome do Jogador: '))
+tot = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+for c in range(0, tot):
+    partidas.append(int(input(f'Quantos gols na partida {c+1}? ')))
+jogador['gols'] = partidas[:]
+jogador['total'] = sum(partidas)
+print('-=' * 30)
+print(jogador)
+print('-=' * 30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 30)
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+for i, v in enumerate(jogador['gols']):
+    print(f'     => Na partida {i+1} fez {v} gols')
+print(f'Foi um total de {jogador["total"]} gols')

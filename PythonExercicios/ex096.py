aaf633cbc5e26e3b60459bf0adb9abf01bb5d03a
Patrick Ericksson <patrickericksson@gmail.com>
2023-07-06 19:32:30 -0300

    Exercises Update

---
+++ PythonExercicios/ex096.py
@@ -0,0 +1,11 @@
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {a}m2')
+
+
+# Programa Principal
+print('Controle de Terrenos')
+print('-' * 20)
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+area(l, c)

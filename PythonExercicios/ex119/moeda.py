4a6e9b436db5b1422c9e9164089fdd5acbda5849
Patrick Ericksson <patrickericksson@gmail.com>
2023-07-12 21:33:42 -0300

    Exercises Update

---
+++ PythonExercicios/ex119/moeda.py
@@ -0,0 +1,34 @@
+def aumentar(preco=0, taxa=0, formato=False):
+    res = preco + (preco * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def diminuir(preco=0, taxa=0, formato=False):
+    res = preco - (preco * taxa / 100)
+    return res if formato is False else moeda(res)
+
+
+def dobro(preco=0, formato=False):
+    res = preco * 2
+    return res if not formato else moeda(res) #Mesmo resultado de cima, mas sintaxe diferente
+
+
+def metade(preco=0, formato=False):
+    res = preco / 2
+    return res if not formato else moeda(res)
+
+
+def moeda (preco=0, moeda='R$'):
+    return f'{moeda}{preco:>.2f}'.replace('.', ',')
+
+
+def resumo(preco=0, taxaa=10, taxar=5):
+    print('-' * 30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-' * 30)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'Com {taxaa}% de aumento: {aumentar(preco, taxaa, True)}')
+    print(f'Com {taxar}% de redução: \t{diminuir(preco, taxar, True)}')
+    print('-' * 30)
